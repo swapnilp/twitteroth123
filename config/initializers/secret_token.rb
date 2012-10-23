cb6286530b8bfdec1a14cbe0405fde1e760a08a9
swapnil <swapnil.patil04@gmail.com>
2012-10-23 07:43:28 +0530

    twitteroth app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitteroth::Application.config.secret_token = '5b527749c9f5b5e1e7d83609e7308948d46013d61b43ec1f78fafeffca375ca464e967f60a2ae92f020b91e5b9061e032926fe1da4e9ca0de161f89ea7ec1818'
