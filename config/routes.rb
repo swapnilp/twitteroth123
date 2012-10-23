d1c90671054863c6eab637db37425e9b91ee1d4e
swapnil <swapnil.patil04@gmail.com>
2012-10-23 09:48:19 +0530

    working on devise views

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Twitteroth::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => 'welcome#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
